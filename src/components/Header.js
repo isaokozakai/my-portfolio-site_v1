ed60ea752a478ddc5dce3763d9bf939f7044cc21
Isao Kozakai <feat130@hotmail.co.jp>
2019-10-02 10:25:11 -0700

    Add smooth scrolling style and the home button function

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,17 @@ const Header = () => {
   return (
     <header className="header">
       <div className="container">
-        <h1 className="logo" alt="Isao"><i className="fas fa-seedling fa-1x" aria-hidden="true"></i></h1>
+        <a href="/">
+          <h1 className="logo" alt="Isao">
+            <i className="fas fa-seedling fa-1x" aria-hidden="true"></i>
+          </h1>
+        </a>
         <nav className="nav-menu">
           <ul>
             <li><a href="#about">About</a></li>
             <li><a href="#projects">Projects</a></li>
             <li><a href="#contact">Contact</a></li>
-          </ul>  
+          </ul>
         </nav>
       </div>
     </header>
