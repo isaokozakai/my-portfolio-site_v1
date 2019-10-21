2abe70f95c723580175af58ab0c844eeb2cb27b8
Isao Kozakai <feat130@hotmail.co.jp>
2019-10-21 12:28:41 -0700

    Add a project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,8 +5,17 @@ const Project = () => (
     <h2 className="headline">Projects</h2>
     <div className="row">
       <div className="col">
-        <div className="card">
-        </div>
+        <a href="https://isao-bulletin-board-service.herokuapp.com">
+          <div className="card">
+            <img src="../../images/Bulletin_Board_App.png" />
+            <p>Technologies</p>
+            <ul>
+              <li>React/Redux</li>
+              <li>Node.js/Express</li>
+              <li>MongoDB</li>
+            </ul>
+          </div>
+        </a>
       </div>
       <div className="col">
         <div className="card">
