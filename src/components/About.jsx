ab541f312eb36adb6c8365e6134c85dadd11687c
isao <feat130@hotmail.co.jp>
2020-01-10 09:50:41 -0800

    Modify the introduction

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,13 +9,15 @@ const About = () => {
       <div className="row">
         <div className="intro">
           <h3>Who I am</h3>
-          <p>My name is Isao Kozakai. I'm a full-stack web developer based in Vancouver.
-          I was born and raised in a middle-sized city in Japan called Niigata.
-          I had been working as a software developer using mainly Java for 5 years.
+          <p>
+            My name is Isao Kozakai. I'm a full-stack web developer based in Vancouver.
+            I was born and raised in a middle-sized city in Japan called Niigata.
+            I had been working as a software developer using mainly Java for 5 years.
             I'm always happy to and willing to learn new technologies, and am confident in my ability to learn quickly.<br />
             Besides programming, I like practicing martial arts.
-            I have practiced a martial art called Jeet Kune Do for 5 years in Japan, which is founded by Bruce Lee.
-            I have been inspired by his words and life since I got to know about him.</p>
+            I have practiced a martial art called Jeet Kune Do for 5 years in Japan, which was founded by Bruce Lee.
+            I have been inspired by his words and life since I got to know about him.
+          </p>
           <h3>How I work</h3>
           <p>I'm a dedicated and detail-oriented team player with responsibility who can work well under pressure.</p>
         </div>
