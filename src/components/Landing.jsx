72c89bfb6b1f1b0050ba6f336c02f6fbc3f985b7
isao <feat130@hotmail.co.jp>
2020-01-17 20:22:18 -0800

    Animate the landing title

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import 'animate.css'
 
 const Landing = () => {
   return (
     <section id="landing" className="container">
-      <div className="landing-title">
+      <div className="landing-title animated rubberBand delay-1s">
         <h1>Isao Kozakai</h1>
         <h2>Full-Stack Web Developer</h2>
         <h2>Vancouver, BC</h2>
