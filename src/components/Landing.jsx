3200b1bcc94d5e4abea2f248ef0801fd85fbd3c9
isao <feat130@hotmail.co.jp>
2020-02-04 08:11:44 -0800

    Make it allow to restart the animation durting the animation

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,15 +3,15 @@ import React, { useRef } from 'react';
 const Landing = () => {
   const landingTitle = useRef();
   const handleClick = () => {
-    landingTitle.current.classList.add("rubberBand");
-  };
-  const handleAnimationEnd = () => {
     landingTitle.current.classList.remove("rubberBand");
     landingTitle.current.classList.remove("delay");
+    setTimeout(() => {
+      landingTitle.current.classList.add("rubberBand");
+    }, 1);
   };
   return (
     <section id="landing" className="container">
-      <div className="landing-title rubberBand delay" onClick={handleClick} onAnimationEnd={handleAnimationEnd} ref={landingTitle}>
+      <div className="landing-title rubberBand delay" onClick={handleClick} ref={landingTitle}>
         <h1>Isao Kozakai</h1>
         <h2>Full-Stack Web Developer</h2>
         <h2>Vancouver, BC</h2>
