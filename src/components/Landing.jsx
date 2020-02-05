8c1e9e9749ce309ac05963676eb970498ea4eb67
isao <feat130@hotmail.co.jp>
2020-02-05 11:32:00 -0800

    Change the landing title animation to use state

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,17 +1,16 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 
 const Landing = () => {
-  const landingTitle = useRef();
+  const [landingTitle, setLandingTitle] = useState("landing-title rubberBand delay")
   const handleClick = () => {
-    landingTitle.current.classList.remove("rubberBand");
-    landingTitle.current.classList.remove("delay");
+    setLandingTitle("landing-title");
     setTimeout(() => {
-      landingTitle.current.classList.add("rubberBand");
-    }, 1);
+      setLandingTitle("landing-title rubberBand");
+    });
   };
   return (
     <section id="landing" className="container">
-      <div className="landing-title rubberBand delay" onClick={handleClick} ref={landingTitle}>
+      <div className={landingTitle} onClick={handleClick}>
         <h1>Isao Kozakai</h1>
         <h2>Full-Stack Web Developer</h2>
         <h2>Vancouver, BC</h2>
