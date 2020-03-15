f0abb259bea414fcf170e57c89566a9a794746e2
isao <feat130@hotmail.co.jp>
2020-03-15 15:23:29 -0700

    Refactor Projects part

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,52 +1,24 @@
 import React from 'react';
 import Project from './partials/Project';
+import projectData from '../assets/projects.json';
 
 const Projects = () => (
   <section id="projects" className="container">
     <h2 className="headline">PROJECTS</h2>
     <div className="grid-container">
-      <Project
-        imageSrc="images/sharedlist.png"
-        imageAlt="sharedlist"
-        technologies={[
-          "React", "Redux", "HTML", "CSS", "Bootstrap", "Node.js", "Express", "MongoDB", "JWT"
-        ]}
-        websiteUrl="https://isao-sharedlist.herokuapp.com/"
-        githubUrl="https://github.com/isaokozakai/mern-bbs"
-      >
-        <p>
-          This is a mockup bulletin board application,
-          which you can register an account, login/logout,
-          and read/create/edit/delete articles.
-        </p>
-      </Project>
-      <Project
-        imageSrc="images/tri-tech-co.png"
-        imageAlt="tri-tech-co"
-        technologies={[
-          "React", "HTML", "CSS", "Sass"
-        ]}
-        websiteUrl="https://lucid-benz-f12be9.netlify.com/"
-        githubUrl="https://github.com/isaokozakai/tri-tech/tree/revamp"
-      >
-        <p>
-          This is a mockup company website using React
-        </p>
-      </Project>
-      <Project
-        imageSrc="images/flight-search.png"
-        imageAlt="flight-search"
-        technologies={[
-          "jQuery", "Ajax", "Web API", "HTML", "CSS"
-        ]}
-        websiteUrl="https://isaokozakai.github.io/flight-search/"
-        githubUrl="https://github.com/isaokozakai/flight-search"
-      >
-        <p>
-          This is a mockup flight ticket search application,
-          which uses the Skyscanner API.
-        </p>
-      </Project>
+      {
+        projectData.map(({ imageSrc, imageAlt, technologies, websiteUrl, githubUrl, description }) => (
+          <Project
+            imageSrc={imageSrc}
+            imageAlt={imageAlt}
+            technologies={technologies}
+            websiteUrl={websiteUrl}
+            githubUrl={githubUrl}
+          >
+            <p>{description}</p>
+          </Project>
+        ))
+      }
     </div>
   </section>
 )
