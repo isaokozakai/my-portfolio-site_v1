e5007a0a15d2c5f0669b759a73115daaa7c91102
isao <feat130@hotmail.co.jp>
2020-03-29 12:41:17 -0700

    Add VisibilitySensor

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import VisibilitySensor from 'react-visibility-sensor';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <VisibilitySensor>
+      <App />
+    </VisibilitySensor>
   </BrowserRouter>,
   document.getElementById('root')
 );
