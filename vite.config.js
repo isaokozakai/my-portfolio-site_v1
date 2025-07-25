b0e3b4b03350b2e68d908e99e5dd7a0f24ef1cc2
Isao Kozakai <isaokozakai@gmail.com>
2025-07-24 22:26:57 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
+  base: '/my-portfolio-site_v1',
   plugins: [react()],
   build: {
     outDir: 'build',
